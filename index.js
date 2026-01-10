5eb5800e6a8c9b571b6e8fb1dfec86e283562e23
aryan singh <aryan@payzli.com>
2026-01-10 13:48:25 +0530

    added axios import

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-import axios from "axios";
+const axios = require("axios");
 require('dotenv').config();
 const config = require('./config');
 const {

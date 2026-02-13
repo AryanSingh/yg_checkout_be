99390bba35c4490420578543584a617b9b22e76f
aryan singh <aryan@payzli.com>
2026-02-13 19:44:55 +0530

    fixed

--- products.js
+++ products.js
@@ -1,12 +1,12 @@
 const products = {
   "100_WITH_ACCOM": {
     name: "100 Hour Yoga API with Accommodation",
-    price: 5300,
+    price: 4850,
     currency: "INR"
   },
   "100_WITHOUT_ACCOM": {
     name: "100 Hour Yoga API without Accommodation",
-    price: 5300,
+    price: 4850,
     currency: "INR"
   },
   "200_WITH_ACCOM": {
